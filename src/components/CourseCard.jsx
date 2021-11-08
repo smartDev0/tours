df45bbf07e85aa58fedb78045c39a8f1254545b7
lucamicheli <lm@customerly.io>
2021-11-08 15:18:51 +0000

    fix: card name

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -3,13 +3,14 @@ import React from "react"
 import PropTypes from "../util/PropTypes"
 import * as styles from "./CourseCard.module.scss"
 import Typography from "./Typography"
+
 const CourseCard = ({ children, url, title, text, tour }) => (
     <div className={classnames([styles.container])}>
         <div className={classnames([styles.img_container])}>
             <img src={url} alt='' className="img-fluid" />
         </div>
         <Typography variant='heading3'>
-            <span>+{tour}</span> {title}
+            {title}
         </Typography>
         <Typography variant="heading6">
             {text}
